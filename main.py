54ddba3e457e53eef5d1e127a7edfda9ccddde4f
Kaustubh <kaustubh8856@gmail.com>
2024-09-08 11:52:49 +0530

    basic ui completed

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request
+import pandas as pd
 app = Flask(__name__)
+data = pd.read_csv('CleanedData.csv')
+
 @app.route('/')
 def index():
-    return render_template("index.html")
+    locations = sorted(data['location'].unique())
+    return render_template("index.html",locations=locations)
 
 if __name__ == "__main__":
     app.run(debug=True,port=5000)
